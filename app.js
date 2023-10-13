9ce599d16f514bb17b026f055c588b108cf55fcb
Deldore <deldore.emil@yandex.ru>
2023-10-13 17:30:42 +0300

    fix bugs

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ for(let i = 0; i < addBtn.length; i++)
     };
     subBtn[i].onclick = function () {
         product[i]--;
+        value[i].innerHTML = product[i];
         if (!product[i]) {
             addBtn[i].innerHTML = "Купить";
             subBtn[i].style.display = "none";
@@ -67,6 +68,6 @@ Telegram.WebApp.onEvent("mainButtonClicked", function () {
 let usercard = document.getElementById('usercard');
 
 let p = document.createElement('p');
-p.innerText = `${tg.initDataUnsafe.first_name}`
-    `${tg.initDataUnsafe.first_name}`
+p.innerText = `${tg.initDataUnsafe.user.first_name}` +
+    `${tg.initDataUnsafe.user.last_name}`
 usercard.appendChild(p);
